9517c2a132b426e1f6af932d423a8d62aa463da3
Mohamed Basem <mohamedbasem518@gmail.com>
2026-08-01 00:11:45 +0300

    Change Variable type to allow telegram to access it

--- app/config.py
+++ app/config.py
@@ -79,7 +79,7 @@ if not GEMINI_API_KEYS:
 GROQ_API_KEY = _require_env("GROQ_API_KEY")
 BOT_TOKEN = _require_env("BOT_TOKEN")
 BOT_CHAT_ID = _require_int_env("BOT_CHAT_ID")
-BOT_CHANNEL_ID = _optional_int_env("BOT_CHANNEL_ID")
+BOT_CHANNEL_ID = os.getenv("BOT_CHANNEL_ID")
 
 TARGET_CHANNELS = _require_channel_ids("TARGET_CHANNEL_IDS")
 
